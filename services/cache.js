3f1dfedb65fc2b15798dc9d92170593474f4490a
TX-9 <jacobleecs@gmail.com>
2020-01-08 21:40:40 -0500

    overwrite Query.prototype.exec

---
+++ services/cache.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const exec = mongoose.Query.prototype.exec;
+
+mongoose.Query.prototype.exec = function () {
+    console.log('ABOUT TO RUN A QUERY');
+
+    console.log(this.getQuery());
+    console.log(this.mongooseCollection.name);
+    
+    return exec.apply(this, arguments);
+}
