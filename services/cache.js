dbead3da3efe6fdbdda986b1de1ceb749bd5482d
TX-9 <jacobleecs@gmail.com>
2020-01-08 21:54:30 -0500

    add cache logic

--- services/cache.js
+++ services/cache.js
@@ -7,13 +7,22 @@ const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
-mongoose.Query.prototype.exec = function () {
+mongoose.Query.prototype.exec = async function () {
     console.log('ABOUT TO RUN A QUERY');
-
     console.log(this.getQuery());
     console.log(this.mongooseCollection.name);
     const key = JSON.stringify(Object.assign({}, this.getQuery(),
         {collection: this.mongooseCollection.name}));
-    console.log(key);
-    return exec.apply(this, arguments);
+
+    // See if there is a value for 'key' in redis
+    const cacheValue = await client.get(key);
+
+    // If do, return that
+    if (cacheValue) {
+        console.log(cacheValue);
+    }
+    // Otherwise, issue the query and store the result in redis
+
+    const result = await exec.apply(this, arguments);
+    console.log(result);
 }
