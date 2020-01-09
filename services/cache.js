37fc71d9acf0fd206d8fc52faec6c49689c6d6eb
TX-9 <jacobleecs@gmail.com>
2020-01-08 21:46:07 -0500

    generate key of cache

--- services/cache.js
+++ services/cache.js
@@ -7,6 +7,8 @@ mongoose.Query.prototype.exec = function () {
 
     console.log(this.getQuery());
     console.log(this.mongooseCollection.name);
-    
+    const key = JSON.stringify(Object.assign({}, this.getQuery(),
+        {collection: this.mongooseCollection.name}));
+    console.log(key);
     return exec.apply(this, arguments);
 }
