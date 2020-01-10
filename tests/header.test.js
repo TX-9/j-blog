73062148a2a906663ecb7d40c57984122bf1f40e
TX-9 <jacobleecs@gmail.com>
2020-01-09 22:09:33 -0500

    launching Chromium instances

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,7 +1,13 @@
+const puppeteer = require('puppeteer');
+
 test('Adds two number', () => {
    const sum = 1 + 1;
-
    expect(sum).toEqual(2);
+});
 
-
+test('launch a brwoer', async () => {
+   const browser = await puppeteer.launch({
+      headless: false
+   });
+   const page = await browser.newPage();
 });
