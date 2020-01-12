9ed781a999518d2b58ad9c861aa1d35e9755e3d6
TX-9 <jacobleecs@gmail.com>
2020-01-12 17:34:27 -0500

    setup Global Jest (Jest setup file)

---
+++ tests/setup.js
@@ -0,0 +1,7 @@
+require('../models/User');
+
+const mongoose = require('mongoose');
+const keys = require('../config/keys');
+
+mongoose.Promise = global.Promise; // Node js global Promise object
+mongoose.connect(kyes.mongoURI, { useMongoClient: true});
