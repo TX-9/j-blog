4e79acfe8c03561f7014ed8c2abf7c703e8b24d3
TX-9 <jacobleecs@gmail.com>
2020-01-13 17:50:42 -0500

    move login to helper

--- tests/header.test.js
+++ tests/header.test.js
@@ -1,6 +1,4 @@
 
-const sessionFactory = require('./factories/sessionFactory');
-const userFactory = require('./factories/userFactory');
 const Page = require('./helpers/page');
 
 let page;
@@ -29,15 +27,6 @@ test('clicking login starts oauth flow', async () => {
 });
 
 test('when signed in, shows logout button', async () => {
-   const user = await userFactory();
-   const { session, sig } = sessionFactory(user);
-
-   console.log(session, sig);
-
-   await page.setCookie({name: 'session', value: session});
-   await page.setCookie({name: 'session.sig', value: sig});
-   await page.goto('localhost:3000');
-   await page.waitFor('a[href="/auth/logout');
 
    const text = await page.$eval('a[href="/auth/logout"]', el => el.innerHTML);
 
