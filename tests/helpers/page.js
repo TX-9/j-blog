6fba43c01080a06b210273cae2fb3010d14afce8
TX-9 <jacobleecs@gmail.com>
2020-01-14 08:21:07 -0500

    add function which returns value by query

--- tests/helpers/page.js
+++ tests/helpers/page.js
@@ -30,9 +30,14 @@ class CustomPage {
 
         await this.page.setCookie({name: 'session', value: session});
         await this.page.setCookie({name: 'session.sig', value: sig});
-        await this.page.goto('localhost:3000');
+        await this.page.goto('localhost:3000/blogs');
         await this.page.waitFor('a[href="/auth/logout');
 
+    }
+
+    async function getCotentsOf(query) {
+        const text = await page.$eval(query, el => el.innerHTML);
+        return text;
     }
 }
 
