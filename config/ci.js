dc617fed8228c337587fc62a7b10e1922246fbb0
TX-9 <jacobleecs@gmail.com>
2020-01-14 15:51:05 -0500

    add project configuration for CI

---
+++ config/ci.js
@@ -0,0 +1,8 @@
+module.exports = {
+    googleClientID:
+        '429102389806-h8ta6fkciacvr818fdst3vuf5ohcs7pj.apps.googleusercontent.com',
+    googleClientSecret: 'DU7K_NzAtvzOpKA-8H-5Tc_k',
+    mongoURI: 'mongodb://127.0.0.1:27017/blog_ci',
+    cookieKey: '123123123',
+    redisUrl: 'redis://127.0.0.1:6379'
+};
