ef1a801288b72ed4be8065e6366feaae5165a81f
TX-9 <jacobleecs@gmail.com>
2020-01-23 07:07:57 -0500

    change mongodb instance

--- config/dev.js
+++ config/dev.js
@@ -1,7 +1,7 @@
 module.exports = {
-  googleClientID:
-    '',
-  googleClientSecret: '',
-  mongoURI: '',
-  cookieKey: ''
+  googleClientID: '429102389806-h8ta6fkciacvr818fdst3vuf5ohcs7pj.apps.googleusercontent.com',
+  googleClientSecret: 'DU7K_NzAtvzOpKA-8H-5Tc_k',
+  mongoURI: 'mongodb+srv://root:P@$$w0rd@cluster0-kewbh.mongodb.net/test?retryWrites=true&w=majority',
+  cookieKey: '123123123',
+  redisUrl: 'redis://127.0.0.1:6379'
 };
