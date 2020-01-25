8a2b5e787875f028c207cdfeaa487a6c9125863d
TX-9 <jacobleecs@gmail.com>
2020-01-25 14:44:51 -0500

    add get list of blogs without login

--- tests/blogs.test.js
+++ tests/blogs.test.js
@@ -75,11 +75,43 @@ describe('User is not logged in', async () => {
         }
     ];
 
-    test('Blog related actions are prohibited', async () => {
-        const results = await page.execRequests(actions);
-        for (let result of results) {
-            expect(result).toEqual({ error: 'You must log in!'});
-        }
+    // test('Blog related actions are prohibited', async () => {
+    //     const results = await page.execRequests(actions);
+    //     for (let result of results) {
+    //         expect(result).toEqual({ error: 'You must log in!'});
+    //     }
+    // });
+
+    test('User cannot create blog posts', async () => {
+        const result = await page.evaluate(
+            () => {
+                return fetch('/api/blogs', {
+                    method: 'POST',
+                    credentials: 'same-origin', // includes cookie to be used
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({ title: 'My Title', content: 'My Content'})
+                }).then(res => res.json());
+
+            }
+        );
+        console.log(result);
+        expect(result).toEqual({ error: 'You must log in!'});
     });
 
+    test('User cannot get a list of posts', async () => {
+       const result = await page.evaluate(
+           () => {
+               return fetch('/api/blogs', {
+                   method: 'GET',
+                   credentials: 'same-origin', // includes cookie to be used
+                   headers: {
+                       'Content-Type': 'application/json'
+                   },
+               }).then(res => res.json());
+           }
+       );
+       expect(result).toEqual({ error: 'You must log in!'});
+    });
 });
