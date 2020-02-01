b23203a66306696f8d11125680edfb81c9d68dc1
TX-9 <jacobleecs@gmail.com>
2020-02-01 10:19:41 -0500

    call getSignedUrl

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,5 +1,7 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
+const uuid = require('uuid/v1');
+const requireLogin = require('../middlewares/requireLogin');
 
 const s3 = new AWS.S3({
     accessKeyId: keys.accessKeyId,
@@ -7,7 +9,14 @@ const s3 = new AWS.S3({
 });
 
 module.exports = app => {
-    app.get('/api/upload', (req, res) => {
-        
+    app.get('/api/upload', requireLogin, (req, res) => {
+        const key = `${req.user.id}/${uuid()}.jpeg`;
+
+        s3.getSignedUrl('putObject', {
+                Bucket: 'j-blog-lee',
+                ContentType: 'jpeg',
+                Key: key
+            },
+            (err, url) => res.send({key, url}));
     });
 };
