b4bd30bd185689879786fa7c13cbd67255c7d383
TX-9 <jacobleecs@gmail.com>
2020-02-01 15:03:49 -0500

    fix accesskey

--- routes/uploadRoutes.js
+++ routes/uploadRoutes.js
@@ -1,22 +1,28 @@
 const AWS = require('aws-sdk');
 const keys = require('../config/keys');
-const uuid = require('uuid/v1');
+const uuid = require('uuid/v4');
 const requireLogin = require('../middlewares/requireLogin');
 
+console.log(keys.accessKeyID, ':::::::::::::', keys.secretAccessKey);
 const s3 = new AWS.S3({
-    accessKeyId: keys.accessKeyId,
+    accessKeyId: keys.accessKeyID,
     secretAccessKey: keys.secretAccessKey
 });
 
 module.exports = app => {
     app.get('/api/upload', requireLogin, (req, res) => {
         const key = `${req.user.id}/${uuid()}.jpeg`;
-
+        console.log(s3);
         s3.getSignedUrl('putObject', {
                 Bucket: 'j-blog-lee',
-                ContentType: 'jpeg',
+                ContentType: 'image/jpeg',
                 Key: key
             },
-            (err, url) => res.send({key, url}));
+            (err, url) =>
+            {
+                console.log(err, url)
+                return res.send({key, url});
+            }
+        );
     });
 };
